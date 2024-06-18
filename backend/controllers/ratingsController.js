0363c9c37a0fa0b723cbdaec666508b321796026
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-06-18 18:49:31 -0400

    working loading reviews from db

--- backend/controllers/ratingsController.js
+++ backend/controllers/ratingsController.js
@@ -3,7 +3,10 @@ const pool = require("../config/database");
 exports.allRatings = async (req,res,next) => {
   const client = await pool.connect();
   try {
-    const result = await client.query("SELECT * FROM Ratings");
+    const result = await client.query(`
+      SELECT r.*, u.username FROM Ratings r
+      JOIN Users u ON r.uid = u.uid
+    `);
     res.status(200).json({data: result.rows});
   } catch (err) {
     console.error(err);
