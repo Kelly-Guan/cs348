50aef6224b5d9e3513877406aa359fd74127a116
s-AndrewJia <andrewkia3@gmail.com>
2024-06-19 18:46:52 -0400

    username and email should be unique

--- sampleData/schema/users.sql
+++ sampleData/schema/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   uid SERIAL UNIQUE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 );
