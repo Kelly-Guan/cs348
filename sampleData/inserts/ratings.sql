1f29d0d7b9da4f114c67a2ac8ab5dae023158770
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-06-19 23:58:05 -0400

    idk

--- sampleData/inserts/ratings.sql
+++ sampleData/inserts/ratings.sql
@@ -1,4 +1,6 @@
-INSERT INTO ratings (uid,mid,score,rating_text,upvotes,downvotes, date_posted) VALUES
-(1,1,2,'This movie is awesome!',3,0,'2024-06-12 13:23:00'),
-(1,2,3,'This movie is subpotimal',2,3, '2024-06-12 16:23:00'),
-(3,2,0,'This movie is awful',0,5,'2024-06-12 19:23:00');
+INSERT INTO users (first_name,last_name,username,email,password) VALUES
+('Andrew','Jia','akyjia','akyjia@uwaterloo.ca','abcd123'),
+('Hudson','Koyanagi','hkoya','hkoya@uwaterloo.ca','bazinga!312'),
+('Kelly','Guan','kgguan','kgguan@uwaterloo.ca','asdfg12357'),
+('Arjun','Walia','a24walia','a24walia@uwaterloo.ca','10283ub'),
+('Lindsay','Zhang','l47zhang','l47zhang@uwaterloo.ca','asubvg18');
