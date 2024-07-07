6738b436c72bbd97ca540858679d27b3437fcb60
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-07-07 17:26:51 -0400

    search implemented

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -6,6 +6,6 @@ router.get("/allMovies", movieController.allMovies);
 router.get("/movie", movieController.movie);
 router.get("/recentReleases", movieController.recentReleases);
 router.get("/popularMovies", movieController.popularMovies);
-router.get("/test", movieController.test);
+router.get("/search", movieController.search);
 
 module.exports = router;
