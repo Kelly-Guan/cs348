53469f331adcd719b8ec5e77c2b62fb3d4102a82
Arjun Walia <arjun.walia@gx.cds.on.ca>
2024-07-09 12:38:58 -0400

    Added UserConnections

--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -5,8 +5,6 @@ const ratingsController = require("../controllers/ratingsController");
 router.get("/allRatings", ratingsController.allRatings);
 router.get("/ratingsByGenre/:genre", ratingsController.ratingsByGenre);
 router.get("/ratingsByRatio/:ratio", ratingsController.ratingsByRatio);
-
-//needs fix
 router.get("/ratingsByFriends/:follower_uid", ratingsController.ratingsByFriends);
 
 module.exports = router;
