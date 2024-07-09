d9446e22aa1673a044e3086077faddc33d50f6fa
Arjun Walia <arjun.walia@gx.cds.on.ca>
2024-07-08 22:48:07 -0400

    Update ratings.js

--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -4,6 +4,8 @@ const ratingsController = require("../controllers/ratingsController");
 
 router.get("/allRatings", ratingsController.allRatings);
 router.get("/ratingsByGenre/:genre", ratingsController.ratingsByGenre);
+
+//below needs fix
 router.get("/ratingsByFriends/:follower_uid", ratingsController.ratingsByFriends);
 router.get("/ratingsByScore/:score", ratingsController.ratingsByScore);
 
