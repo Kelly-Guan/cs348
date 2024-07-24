8f7fdcc92fd7dc4021ca1dac671d4e09ceb54f30
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-07-24 17:20:04 -0400

    trying to fix search page

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,37 +1,19 @@
 import React, { useState, useEffect } from "react";
 import MovieButton from "../components/ui/movieButton";
 import SearchBar from "../components/ui/searchBar";
-import movieFiller from "../assets/littleWomen.jpg";
-import movieFillerVer from "../assets/fillerVert.jpg";
+import { posterLinkToImgURL } from "../utils";
 import GenreDropdown from "../components/genreDropDown";
 
 const genres = ["Comedy", "Horror", "Drama", "Romance", "Action", "Science Fiction"]; // Add more genres as needed
 const runtimes = ["short", "medium", "long"];
-const ratings = [1, 2, 3, 4, 5];
+const ratings = ['1-2', '2-3', '3-4', '4-5'];
 
 function SearchPage() {
   const [movies, setMovies] = useState([]);
-
-  const [movieCast, setMovieCast] = useState([]);
-  const [movieGenre, setMovieGenre] = useState([]);
-
   const [search, setSearch] = useState("");
   const [genre, setGenre] = useState("");
   const [rating, setRating] = useState("");
   const [runtime, setRuntime] = useState("");
-
-  const checkImageURL = async (url, defaultURL) => {
-    try {
-      const response = await fetch(url);
-      if (response.status === 200) {
-        return url;
-      } else {
-        return defaultURL;
-      }
-    } catch (error) {
-      return defaultURL;
-    }
-  };
 
   const selectGenre = (genre) => {
     setGenre(genre);
@@ -46,32 +28,19 @@ function SearchPage() {
     setSearch(search);
   };
 
-  // useEffect(() => {
-  //   fetch("http://localhost:3001/api/movies/allMovies")
-  //     .then((res) => {
-  //       if (!res.ok) {
-  //         throw new Error(`HTTP error! Status: ${res.status}`);
-  //       }
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setMovies(data.data);
-  //     })
-  //     .catch((err) => {
-  //       console.error("Fetch error:", err);
-  //       setMovies([]);
-  //     });
-  // }, []);
-
   useEffect(() => {
     let tempMovies = [];
     let query = new URLSearchParams();
     if (search) query.append("title", search);
     if (genre) query.append("genre", genre);
-    // if(rating) query.append('rating', rating);
+    if(rating) query.append('rating', rating);
     if (runtime) query.append("runtime", runtime);
     // query.append("offset", 50);
+
+    const do_thing = async (link) => {
+      let val = await posterLinkToImgURL(link);
+      return val;
+    }
 
     fetch(`http://localhost:3001/api/movies/search?${query.toString()}`)
       .then((res) => {
@@ -83,6 +52,7 @@ function SearchPage() {
       .then((data) => {
         tempMovies = data;
         for(let i = 0; i < data.length; i++) {
+          tempMovies[i].poster_link = do_thing(tempMovies[i].poster_link);
           tempMovies[i].cast = []
           tempMovies[i].genres = []
         }
@@ -101,6 +71,8 @@ function SearchPage() {
               tempMovies[i].genres = data;
               tempMovies[i].cast = dat2;
               setMovies(tempMovies);
+              console.log(tempMovies)
+
             } catch (error) {
               console.error("Error fetching movie genres:", error);
             }
@@ -135,18 +107,7 @@ function SearchPage() {
         </div>
         <div className="mb-20 w-full">
           <div className="grid gap-x-3 gap-y-10 grid-cols-3">
-            {/* <MovieButton
-              movieImg={movieFiller}
-              movieImgVert={movieFillerVer}
-              movieTitle="The GodFather"
-              movieTime="2004 2h 17m"
-              movieDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed egestas. Consequat..."
-              movieCast="Uma Thurman, David Carradine, Micheal, more"
-              movieGenre="Martial Arts Move, Action, Adventure"
-              alt="Movie Poster"
-              pageType="search"
-            /> */}
-            {movies == [] ? (
+            {/* {movies == [] ? (
               <></>
             ) : (
               movies.map((m) => (
@@ -162,7 +123,20 @@ function SearchPage() {
                   pageType="search"
                 />
               ))
-            )}
+            )} */}
+            {movies.map((m) => (
+                <MovieButton
+                  movieImg={m.poster_link}
+                  movieImgVert={m.poster_link}
+                  movieTitle={m.title}
+                  movieTime={m.runtime}
+                  movieDescription={m.description}
+                  movieCast={m["cast"].slice(0,5).toString()}
+                  movieGenre={m["genres"].toString()}
+                  alt={m.title}
+                  pageType="search"
+                />
+              ))}
           </div>
         </div>
       </div>
